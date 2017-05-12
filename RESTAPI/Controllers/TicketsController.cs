bd81c72506973a3c545d7af6d15b287fce48abf1
ppittic <ppittic@yahoo.com>
2017-05-12 18:53:47 +0300

    Add project files.

---
+++ RESTAPI/Controllers/TicketsController.cs
@@ -0,0 +1,56 @@
+﻿using RESTAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//*****************************************
+// In Web API, a controller is an object that handles HTTP requests.
+// How the routing works: When the Web API framework receives a request, it routes the request to an action.
+// To determine which action to invoke, the framework uses a routing table - default in WebApiConfig.cs
+// The default route template for Web API is "api/{controller}/{id}". 
+// In this template, "api" is a literal path segment, and {controller} and {id} are placeholder variables.
+// When the Web API framework receives an HTTP request, it tries to match the URI against one of the route templates in the routing table.
+// If no route matches, the client receives a 404 error.
+// Once a matching route is found, Web API selects the controller and the action:
+// To find the controller, Web API adds "Controller" to the value of the {controller} variable.
+// To find the action, Web API looks at the HTTP method, and then looks for an action whose name begins with that HTTP method name.
+// For example, with a GET request, Web API looks for an action that starts with "Get...", such as "GetContact" or "GetAllContacts". 
+// This convention applies only to GET, POST, PUT, and DELETE methods.
+// You can enable other HTTP methods by using attributes on your controller.
+// Other placeholder variables in the route template, such as {id}, are mapped to action parameters.
+
+//LocalDB - (localdb)\MSSQLLocalDB
+//*****************************************
+namespace RESTAPI.Controllers
+{
+    public class TicketsController : ApiController
+    {
+        Ticket[] tickets = new Ticket[]
+       {
+            new Ticket { Id = 1, Reporter="Gogu 1", Message = "Tomato Soup"},
+            new Ticket { Id = 2, Reporter="Gogu 2", Message = "Fluffy panda"},
+            new Ticket { Id = 3, Reporter="Gogu 3", Message = "Pink unicorns"}
+       };
+        //*****************************************
+        //Controller Method   URI - GetAllTickets	/api/tickets
+        //*****************************************
+        public IEnumerable<Ticket> GetAllTickets()
+        {
+            return tickets;
+        }
+        //*****************************************
+        //Controller Method   URI - GetTicket	/api/tickets/id
+        //*****************************************
+        public IHttpActionResult GetTicket(int id)
+        {
+            var ticket = tickets.FirstOrDefault((p) => p.Id == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            return Ok(ticket);
+        }
+    }
+}
---
+++ RESTAPI/Models/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//*****************************************
+// A model is an object that represents the data in your application.
+// ASP.NET Web API can automatically serialize your model to JSON, XML, or some other format, 
+// and then write the serialized data into the body of the HTTP response message. 
+// As long as a client can read the serialization format, it can deserialize the object. 
+// Most clients can parse either XML or JSON. 
+// Moreover, the client can indicate which format it wants by setting the Accept header in the HTTP request message.
+//*****************************************
+namespace RESTAPI.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Reporter { get; set; }
+        public string Message { get; set; }
+      
+       // public string Assignee { get; set; }
+       // public int Status { get; set; }
+       // public int Priority { get; set; }
+       // public int Category { get; set; }
+    }
+}
