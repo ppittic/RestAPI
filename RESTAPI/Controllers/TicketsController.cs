c25f73283e006cc85bfc63a1bedf5a9e6f202e8b
ppittic <ppittic@yahoo.com>
2017-05-31 14:33:54 +0300

    Cleanup - done

--- RESTAPI/Controllers/TicketsController.cs
+++ RESTAPI/Controllers/TicketsController.cs
@@ -61,61 +61,100 @@ namespace RESTAPI.Controllers
             }
             catch (Exception e)
             {
-                string errMessage = e.Message;  //Debug purposes
-
-                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                {
-                    Content = new StringContent("An error occurred, please try again or contact the administrator."),
-                    ReasonPhrase = "Critical Exception"
-                });
+                return Content(HttpStatusCode.InternalServerError, e.Message);
             }
         }
         [HttpPut]
         public IHttpActionResult PutTicket(int id, string reporter, string message)
         {
-            var ticket = new TICKET(id, reporter, message);
-            db.TICKETS.Add(ticket);
-            db.SaveChanges();
-            return Ok(ticket);
+            try
+            {
+                //Use Put to create or replace
+                var ticket = db.TICKETS.FirstOrDefault((p) => p.Id == id);
+                if (ticket == null)
+                {
+                    ticket = new TICKET(id, reporter, message);
+                    db.TICKETS.Add(ticket);
+                    db.SaveChanges();
+                    return Ok(ticket);
+                }
+                else
+                {
+                    ticket.Message = message;
+                    ticket.Reporter = reporter;
+                    db.TICKETS.Attach(ticket);
+                    var entry = db.Entry(ticket);
+                    entry.Property(e => e.Message).IsModified = true;
+                    entry.Property(e => e.Reporter).IsModified = true;
+                    db.SaveChanges();
+
+                    return Ok(entry);
+                }
+            }
+            catch (Exception e)
+            {
+                return Content(HttpStatusCode.InternalServerError, e.Message);
+            }
         }
         [HttpPost]
         public IHttpActionResult PostTicket(int id, string reporter, string message)
         {
-            var ticket = new TICKET(id, reporter, message);
-            db.TICKETS.Add(ticket);
-            db.SaveChanges();
-            return Ok(ticket);
+            try
+            {
+                var ticket = new TICKET(id, reporter, message);
+                db.TICKETS.Add(ticket);
+                db.SaveChanges();
+                return Ok(ticket);
+            }
+            catch (Exception e)
+            {
+                return Content(HttpStatusCode.InternalServerError, e.Message);
+            }
         }
-    [HttpPatch]
-    public IHttpActionResult PatchTicket(int id, string message)
-    {
-        var ticket = db.TICKETS.Find(id);
-        if (ticket != null)
+        [HttpPatch]
+        public IHttpActionResult PatchTicket(int id, string message)
         {
-            ticket.Message = message;
-            db.TICKETS.Attach(ticket);
-            var entry = db.Entry(ticket);
-            entry.Property(e => e.Message).IsModified = true;
-            db.SaveChanges();
+            try
+            {
+                var ticket = db.TICKETS.Find(id);
+                if (ticket != null)
+                {
+                    ticket.Message = message;
+                    db.TICKETS.Attach(ticket);
+                    var entry = db.Entry(ticket);
+                    entry.Property(e => e.Message).IsModified = true;
+                    db.SaveChanges();
 
-            return Ok(entry);
+                    return Ok(entry);
+                }
+                else throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            catch (Exception e)
+            {
+                return Content(HttpStatusCode.InternalServerError, e.Message);
+            }
         }
-        else return Content(HttpStatusCode.BadRequest, "Error");
-    }
-    [HttpDelete]
-    public HttpResponseMessage DeleteTicket(int id)
-    {
-        var ticket = db.TICKETS.Find(id);
-        if (ticket != null)
+        [HttpDelete]
+        public HttpResponseMessage DeleteTicket(int id)
         {
-            db.TICKETS.Remove(ticket);
-            db.SaveChanges();
-            var response = new HttpResponseMessage();
-            response.Headers.Add("DeleteMessage", "Succsessfuly Deleted!!!");
-            return response;
+            try
+            {
+                var ticket = db.TICKETS.Find(id);
+                if (ticket != null)
+                {
+                    db.TICKETS.Remove(ticket);
+                    db.SaveChanges();
+                    var response = new HttpResponseMessage();
+                    response.Headers.Add("DeleteMessage", "Succsessfuly Deleted!!!");
+                    return response;
+                }
+                else
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
         }
-        else
-            throw new HttpResponseException(HttpStatusCode.NotFound);
-    }
     }
 }
---
+++ RESTAPI/Scripts/APIRequests.js
@@ -0,0 +1,117 @@
+﻿var uri = 'api/tickets';
+
+function formatItem(item) {
+    return item.Reporter + ': "' + item.Message + '"';
+}
+
+function showAll() {
+    // Send an AJAX request
+    $.getJSON(uri)
+        .done(function (data) {
+            // On success, 'data' contains a list of products.
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+                $("#tickets").append('<li class="list-group-item">' + '<span class="badge pull-left">' + item.Id + '</span>' + formatItem(item) + '</li>');
+
+                $('#AllTickets').show();
+            });
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            if (jqXHR.status >= 400 && jqXHR.status < 500)
+                alert("Client Error");
+            else
+                if (jqXHR.status > 500)
+                    alert("Server Error");
+                else
+                    alert('getJSON request failed! ' + "err:" + err);
+        });
+}
+
+function find() {
+    var id = $('#ticketId').val();
+    /*$.ajaxSetup({
+        headers: {
+        'Accept-Encoding': 'gzip'
+       }
+     });
+    */
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            $('#ticket').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            if (jqXHR.status == 404) {
+                alert("404 Not Found");
+            } else {
+                alert("Other non-handled error type");
+            }
+        });
+}
+
+function deleteTicket() {
+    var id = $('#ticketId').val();
+    $.ajax({
+        url: uri + '/' + id,
+        type: 'DELETE',
+        success: function (result) {
+            $('#ticket').text('Item with ID '+ id + ' deleted');
+        },
+        fail: function (jqXHR, textStatus, err) {
+            alert('Error: ' + err + "; Text status" + textStatus);
+        }
+    });
+}
+//POST used to create
+function postTicket() {
+    var id = $('#ticketId').val();
+    var reporter = $('#Reporter').val();
+    var message = $('#Message').val();
+    $.ajax({
+        url: uri + '/' + id + '/' + reporter + '/' + message,
+        type: 'POST',
+        success: function (result) {
+            $('#ticket').text('Item with reporter ' + reporter + ' and message: ' + message + ' POSTED ' + ' ID ' + result.Id);
+        },
+        fail: function (jqXHR, textStatus, err) {
+            alert('Error: ' + err + "; Text status" + textStatus);
+        }
+    });
+}
+//PUT is idempotent (PUTting an object twice has no effect)
+//Used to create a resource, or overwrite it
+function putTicket() {
+    var id = $('#ticketId').val();
+    var reporter = $('#Reporter').val();
+    var message = $('#Message').val();
+    $.ajax({
+        url: uri + '/' + id + '/' + reporter + '/' + message,
+        type: 'PUT',
+        success: function (result) {
+            $('#ticket').text('Item with reporter ' + reporter + ' and message: ' + message + ' PUT ' + 'ID ' + result.Id);
+        },
+        fail: function (jqXHR, textStatus, err) {
+            alert('Error: ' + err + "; Text status" + textStatus);
+        }
+    });
+}
+//
+function patchTicket() {
+    var id = $('#ticketId').val();
+    var message = $('#Message').val();
+    $.ajax({
+        url: uri + '/' + id + '/' + message,
+        type: 'PATCH',
+        success: function (result) {
+            $('#ticket').text('Item with ID '+ id + ' patched. New message: ' + message);
+        },
+        fail: function (jqXHR, textStatus, err) {
+            alert('Error: ' + err + "; Text status" + textStatus);
+        }
+    });
+}
+
+function clearAllTickets()
+{
+    $("#tickets").empty();
+    $('#AllTickets').hide();
+}
